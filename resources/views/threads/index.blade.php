52c9a36667f85eacea893a0aa3c2e922c72c17f7
Auðunn Ezra Kristinsson Simpson <FS453488@fss.is>
2017-09-08 09:40:02 +0000

    Vefforritun tv1 fh

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -5,11 +5,12 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">THE DARK KNIGHT RISES</div>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
+                <ul class="list-group">
+                    <li class="list-group-item">by Jonathan Nolan and Christopher Nolan Story by Christopher Nolan & David S. Goyer Based upon characters appearing in comic books published by DC Comics Batman created by Bob Kane Transcribed to PDF from: "The Dark Knight Trilogy - The Complete Screenplays" Published July 2012 by Faber & Faber Ltd. (UK) In memory of the victims of the tragedy in Aurora, CO, on July 20, 2012. FOR EDUCATIONAL PURPOSES ONLY</li>
+                    <li class="list-group-item">BLACK SCREEN. GORDON (V.O) Harvey Dent was needed. He was everything Gotham has been crying out for. He was...a hero. Not the hero we deserved - the hero we needed. Nothing less than a knight, shining... The sound of cracking. Splintering. A shape appears, in ice. The shape of a BAT. The ice disintegrates... EXT. GOTHAM STREET - DAY Gordon stands before a massive picture of Harvey Dent. GORDON But I knew Harvey Dent. I was...his friend. And it will be a very long time before someone inspires us the way he did. Gordon, choked with emotion, gathers the papers of his eulogy. I believed in Harvey Dent. And we FADE TO BLACK. CUT TO: Racing along a cratered dirt road, and we are - INT. LAND CRUISER JOSTLING OVER UNEVEN TERRAIN - DAY Three Hooded Men guarded by East European Militia. A third Militia drives. Next to him is a nervous, bespectacled man. EXT. AIRSTRIP, EASTERN EUROPE - DAY An airstrip overlooking a grey city rocked by artillery fire. A bland CIA Operative, flanked by Special Forces Men, stands in front of a commuter plane. CIA Man watches the Land Cruiser pull up, hard. The Militia Men jump out of the vehicle. The Driver shoves the bespectacled man in front of the CIA Man.</li>
+                </ul>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,14 @@
 
 Auth::routes();
 
-/*Route::get('/threads', function() {
+Route::get('/threads', function() {
 	return view('threads.index');
 });
 
 Route::get('/threads/create', function () {
 	return view('threads.create');
 });
-*/
 
-Route::get('/', function () {
-	return redirect('home');
+Route::get('/', function(){
+	return view('greeting');
 });
-Route::get('/home', function () {
-	return("home");
-});
-Route::get('/threads', 'ThreadsController@index');
-Route::get('/threads/create', 'ThreadsController@create');
-Route::get('/threads/{id}', 'ThreadsController@show');
