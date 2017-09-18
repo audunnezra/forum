e4681dcbfb68fbdbdd290a0f872423c7c8faf584
Auðunn Ezra Kristinsson Simpson <FS453488@fss.is>
2017-09-18 15:19:34 +0000

    DB facades

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
 {
     public function index(){
+    	$threads = DB::select('select * from threads');
 
-		return view('threads.index');
+		return view('threads.index', compact('threads'));
 
 	}
 
@@ -20,7 +22,7 @@ class ThreadsController extends Controller
 
 	public function show($id){
 
-		
+		$thread = DB::select('select * from threads where id=?'))
 
 		return view('threads.show');
 	}
