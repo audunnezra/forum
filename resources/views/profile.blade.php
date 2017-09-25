6d83d6a30fe32cc768e582f74dd10d06473e3dcc
Auðunn Ezra Kristinsson Simpson <FS453488@fss.is>
2017-09-25 15:15:16 +0000

    migrations

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
+	<div class="row"> 
 		<div class="panel panel-default">
 		  <div class="panel-heading">Panel heading without title</div>
 		  <div class="panel-body">
